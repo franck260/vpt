19f57cfc92b5e86450da171a1a4b223c342f7a72
Franck <franck.perez@gmail.com>
2011-06-10 17:18:15 +0200

    Date-related utility methods

---
+++ app/tests/utils/test_dates.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from app.utils.dates import change_timezone
+import datetime
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+
+class TestDates(unittest.TestCase):
+
+    def test_change_timezone(self):
+        
+        source_date = datetime.datetime(2002, 10, 27, 6, 0, 0)        
+        self.assertEquals(change_timezone(source_date).strftime("%d/%m/%y %H:%M"), "27/10/02 15:00")
+        
+
+
+    
---
+++ app/utils/dates.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+""" Date-related utility methods """
+
+from pytz import timezone
+
+PACIFIC = timezone("US/Pacific-New")
+PARIS = timezone("Europe/Paris")
+
+#TODO: store all dates in UTC and remove the hard-coded references here !
+
+def change_timezone(date, source_timezone = PACIFIC, target_timezone = PARIS):
+    """ Returns a new date based on the timezones provided """
+    return source_timezone.localize(date).astimezone(target_timezone)
