6a155e503a1d1d7a8b13b699ea10e0975a0a1fe9
Franck <franck.perez@gmail.com>
2011-07-12 20:54:43 +0200

    WSGI wrapper

---
+++ passenger_wsgi.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+""" WSGI wrapper (see http://wiki.dreamhost.com/Passenger_WSGI) """
+
+# On-the-fly interpreter change
+import sys, os
+INTERP = "/home/franck260/ENV/bin/python"
+if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+sys.path.append(os.getcwd())
+
+# Application configuration
+import application as application_module
+application_module.app.configure("production.cfg")
+
+# The file must export a WSGI server with the name application
+application = application_module.app.wsgifunc() #@UndefinedVariable
