75ee254364b82b0b47e721ac7a7e66c6302fb389
Franck <franck.perez@gmail.com>
2011-08-30 00:03:38 +0200

    Better formatting.

--- app/tests/utils/test_dates.py
+++ app/tests/utils/test_dates.py
@@ -12,7 +12,7 @@ class TestDates(unittest.TestCase):
 
     def test_change_timezone(self):
         
-        source_date = datetime.datetime(2002, 10, 27, 6, 0, 0)        
+        source_date = datetime.datetime(2002, 10, 27, 6, 0, 0)
         self.assertEquals(change_timezone(source_date).strftime("%d/%m/%y %H:%M"), "27/10/02 15:00")
         
 
