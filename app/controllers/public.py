421207da658d610783541d7d4b2752e645e9abb7
Franck <franck.perez@gmail.com>
2011-08-29 20:51:35 +0200

    Improved formatting.

--- app/controllers/public.py
+++ app/controllers/public.py
@@ -4,18 +4,17 @@ from app.utils import session
 import mimetypes
 import web
 
-
 class Public:
     
     @session.configure_session(enabled = False)
     def GET(self): 
         try:
             path = web.ctx.path
-            file_name = path.split('/')[-1]
-            web.header('Content-type', mime_type(file_name))
-            return open(path[1:], 'rb').read()
+            file_name = path.split("/")[-1]
+            web.header("Content-type", mime_type(file_name))
+            return open(path[1:], "rb").read()
         except IOError:
             raise web.notfound()
             
 def mime_type(filename):
-    return mimetypes.guess_type(filename)[0] or 'application/octet-stream' 
+    return mimetypes.guess_type(filename)[0] or "application/octet-stream" 
