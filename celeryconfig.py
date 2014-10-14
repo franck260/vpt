42d7bbae21d835dd698849e612d837c45e36d2d0
Franck <franck.perez@gmail.com>
2014-10-14 17:49:45 +0200

    Updated Celery config.

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,4 +1,4 @@
-BROKER_TRANSPORT = "sqlakombu.transport.Transport"
-BROKER_HOST = "sqlite:///vpt_tasks.db"
-CELERY_IMPORTS = ("app.notifications.handlers", )
+BROKER_URL = "sqla+sqlite:///vpt_tasks.db"
+CELERY_ACCEPT_CONTENT = ["pickle"]
 CELERY_IGNORE_RESULT = True
+CELERY_IMPORTS = ("app.notifications.handlers", )
