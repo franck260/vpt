7ceba0313744be17ecffad0c0eed39ae2ea64b0e
Franck <franck.perez@gmail.com>
2014-10-14 12:37:24 +0200

    Cleaner WSGI wrapper.

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,15 +2,22 @@
 
 """ WSGI wrapper (see http://wiki.dreamhost.com/Passenger_WSGI) """
 
+import os
+import sys
+
+
+# Environment-related settings
+INTERP = os.path.join(os.environ["HOME"], "ENV", "bin", "python")
+CONFIGURATION_FILE = "production.cfg"
+
 # On-the-fly interpreter change
-import sys, os
-INTERP = "/home/franck260/ENV/bin/python"
-if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
 sys.path.append(os.getcwd())
 
 # Application configuration
 import application as application_module
-application_module.app.configure("production.cfg")
+application_module.app.configure(CONFIGURATION_FILE)
 
 # The file must export a WSGI server with the name application
 application = application_module.app.wsgifunc() #@UndefinedVariable
